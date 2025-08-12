f81e5a62106aff7f03d5dcad5e6cdda22091836c
Aladdin Sonni <aladdin.sonni@gmail.com>
2025-08-12 12:39:49 +0200

    08: Updating State

--- lib/providers/cart_provider.dart
+++ lib/providers/cart_provider.dart
@@ -17,6 +17,25 @@ class CartNotifier extends Notifier<Set<Product>> {
   }
 
   // Methods to update state
+  void addProduct(Product product) {
+    // check if product is already in cart
+    if (state.contains(product)) {
+      // if it is, do nothing
+      return;
+    }
+    // add product to cart
+    state = {...state, product};
+  }
+
+  void removeProduct(Product product) {
+    // check if product is in cart
+    if (!state.contains(product)) {
+      // if it isn't, do nothing
+      return;
+    }
+    // remove product from cart
+    state = {...state}..remove(product);
+  }
 }
 
 final cartNotifierProvider = NotifierProvider<CartNotifier, Set<Product>>(() {
